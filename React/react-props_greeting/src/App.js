4e4955218d331002d0f700b544bbe31eedd8ea3c
Anna von Oesen <155978924+avoesen@users.noreply.github.com>
2024-02-02 16:39:04 +0100

    change

--- React/react-props_greeting/src/App.js
+++ React/react-props_greeting/src/App.js
@@ -1,9 +1,9 @@
 import "./styles.css";
 
 export default function App() {
-  return <Greeting name="Roland"/>;
+  return <Greeting name="Coach"/>;
 }
 
 function Greeting({name}) {
-  return (<p>Hello, {name}!</p>)
+  return (<p>Hello, {name==="Coach"?"Roland":"Anna"}!</p>)
 }

a6b4500cdfaa8a2796b35d1f84c13a416d62392f
Anna von Oesen <155978924+avoesen@users.noreply.github.com>
2024-02-16 12:01:19 +0100

    change

--- Next-JS/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ Next-JS/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
 import useSWR from "swr";
@@ -9,8 +8,7 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 export default function ISSTracker() {
   const { data: coords, error, isLoading, mutate } = useSWR(URL, fetcher, {
     refreshInterval: 5000,
-  }); 
-  // const coords = data
+  });
 
   if(error) {
     return <h1>failed</h1>
@@ -19,44 +17,13 @@ export default function ISSTracker() {
     return <h1>...is loading</h1>
   }
 
-  function handleReload() {
-    mutate(); 
-  }
-
-//   const [coords, setCoords] = useState({
-//     longitude: 0,
-//     latitude: 0,
-//   });
-
-//   async function getISSCoords() {
-//     try {
-//       const response = await fetch(URL);
-//       if (response.ok) {
-//         const data = await response.json();
-//         setCoords({ longitude: data.longitude, latitude: data.latitude });
-//       }
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
-
-//   useEffect(() => {
-//     const timer = setInterval(() => {
-//       getISSCoords();
-//     }, 5000);
-
-//     return () => {
-//       clearInterval(timer);
-//     };
-//   }, []);
-
   return (
     <main>
       <Map longitude={coords.longitude} latitude={coords.latitude} />
       <Controls
         longitude={coords.longitude}
         latitude={coords.latitude}
-        onRefresh={() => handleReload()}
+        onRefresh={() => mutate()}
       />
     </main>
   );

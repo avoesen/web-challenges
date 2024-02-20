02a07f9434f35087cc55552caeb574b73db21833
Anna von Oesen <155978924+avoesen@users.noreply.github.com>
2024-02-20 11:06:51 +0100

    Backend-basics

--- Backend/backend-basics_chance/server.js
+++ Backend/backend-basics_chance/server.js
@@ -2,15 +2,21 @@ import { createServer } from "node:http";
 import Chance from "chance";
 
 const chance = new Chance()
-// const {name(), age(), profession()} = chance
 
 const name = chance.name()
 const age = chance.age()
 const profession = chance.profession()
-console.log(chance)
 
 export const server = createServer((request, response) => {
-    response.end(
-      `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
-    );
+    if(request.url === "/") {
+        response.statusCode = 200
+        response.end(
+          `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+        );
+        return
+    } else { 
+        response.statusCode = 404 
+        response.end("Not found")
+        return
+    }
 })

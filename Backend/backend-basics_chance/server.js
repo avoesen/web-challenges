e8fb7bb30d976ed9d67df5b7a0fcd113c01bc4df
Anna von Oesen <155978924+avoesen@users.noreply.github.com>
2024-02-20 11:31:43 +0100

    backend basics

--- Backend/backend-basics_chance/server.js
+++ Backend/backend-basics_chance/server.js
@@ -8,7 +8,7 @@ const age = chance.age()
 const profession = chance.profession()
 
 export const server = createServer((request, response) => {
-    if(request.url === "/") {
+    if(request.url) {
         response.statusCode = 200
         response.end(
           `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
